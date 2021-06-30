5ec513a25fca5671f514b76b3106b1550535eddc
Caique Alves <54457262+icka64136@users.noreply.github.com>
2021-06-30 14:29:12 -0300

    Add files via upload
    C#

---
+++ Produto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loja
+{
+    class Produto
+    {
+        // Ordem  de Organização do Programa: 1° Atributo Privado !
+        private string _nome; // Encapsulamento // o Nome Não Pode Ter Autro Properties neste Caso Pois Ha uma Lógica Por Trás dele !
+        // 2° Propriedades Auto Emplementadas.
+        public double Preco { get; private set; }//Auto Properties Pode Adicionar o Valor mas com o Private Set Não Pode Alterar o Valor!
+        public int Quantidade { get; set; }
+        //3° Construtores
+        public Produto()//CONSTRUTOR
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade) // Construtor tem o mesmo Nome que a Classe .
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        // 4° Propriedades Customizadas
+        public string Nome // Properties Manual
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1) // só faz a Atribuição se o nome Recebido não for Nulo e Tiver mais de um Caracteres.
+                {
+                    _nome = value; // Value é o Parâmetro Passado Para o Metodo Set fazendo a Atribuição.
+                }
+            }
+        }
+        //5° Outros Metodos
+
+        /*public double Preco
+        {
+            get { return _preco; }//Autorização Para o Usuario Exibir o Preço do Produto
+            set { _preco = value; }//Autorização Para o Usuario Modificar o Preço do Produto
+        }*/
+        /*public int Quantidade
+        {
+            get { return _quantidade; }//Autorização Para o Usuario Exibir a Quantidade do Produto
+            set { _quantidade = value; } //Autorização Para o Usuario Modificar a Quantidade do Produto           
+        }*/
+        public double ValorTotal()
+        {
+
+            return Preco * Quantidade;
+        }
+        public void AdicionarProduto(int quantidade)// a Função vai ser Void Pous não vai retornar valor de Saida
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        public override string ToString() // Serve para Fazer a Re Ultilização  dO Código, e Deixar o Programa Mais Limpo.
+        {
+            return "Produto : " + _nome + " , Preço : $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+               + ", Quantidade : " + Quantidade + ", Valor Total : $" + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+
+namespace Loja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite Dados do Produto! , Nome , Preço e Quantidade");
+            string[] v = Console.ReadLine().Split(',');
+            string nome = v[0];
+            double preco = double.Parse(v[1], CultureInfo.InvariantCulture);
+            int quantidade = int.Parse(v[2], CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco, quantidade);//Parâmetros do Construtor
+
+            /*p.Nome = nome; // Properties mais Intuitivo e Didatico . // Possibilidades de Atrinuição Não está sendo Ultilizado pois o Construtor
+            p.Preco = preco; // Ja está recebendo os Parâmetros!
+            p.Quantidade = quantidade;*/
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Estoque : " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite Quantos Produtos Devem Ser Adicionados ao Estoque :");
+            quantidade = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque Atualizado!");
+
+            Console.WriteLine();
+            Console.Write(p);
+            Console.WriteLine();
+
+            Console.Write("Digite Quantos Produtos Irão Sair do Estoque :");
+            quantidade = int.Parse(Console.ReadLine());
+            p.RemoverProduto(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Estoque Atualizado!");
+
+            Console.WriteLine();
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            Console.WriteLine("Digitou Algo Errado ? Qual deseja Corrigir :");
+            Console.Write("Digite 1 - Nome, 2 - Quantidade  ou 0 - Para Sair :");
+            Console.WriteLine();
+            int concertar = int.Parse(Console.ReadLine());
+
+            while (concertar != 0)
+            {
+                switch (concertar)
+                {
+                    case 1:
+                        Console.Write("Corrigir Nome :");
+                        nome = Console.ReadLine();
+                        p.Nome = nome;
+                        break;
+                    case 2:
+                        Console.WriteLine("Corrigir Quantidade :");
+                        quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        p.Quantidade = quantidade;
+                        break;
+                    default:
+                        Console.WriteLine("Opção Invalida !");
+                        break;
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("Novos Dados:" + p);
+
+                Console.WriteLine();
+                Console.Write("Digite 1 - Nome, 2 - Quantidade  ou 0 - Para Sair :");
+                Console.WriteLine();
+                concertar = int.Parse(Console.ReadLine());
+            }
+
+
+
+
+        }
+    }
+}
